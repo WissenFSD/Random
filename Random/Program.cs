9d446a8efd7e001c7f31c6c1c1793fd417467232
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-06 13:30:37 +0300

    Add project files.

---
+++ Random/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// c# dilinde random sayı üretmek isteyebiliriz. Random sayı üretme işini Random ile yapabiliriz
+
+
+Random rnd=new Random();
+
+//  0 ile 10000 arası sayı üretecek
+int randomSayi = rnd.Next(10000);
+
+// her hangi bir sınır vermezseniz int değerinin sınıfları çerçevesinde değerler üretecektir.
+int randomSayi1 = rnd.Next();
+
+// 10 ile 20 arasında bir random sayı üretecektir
+int randomSayi2 = rnd.Next(10, 20);
+
+Console.WriteLine(randomSayi);
+
+
+
+Console.WriteLine(randomSayi1);
+
+Console.WriteLine(randomSayi2);
